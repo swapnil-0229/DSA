db49dc5dbd2dc1ccabd62fd8a1107403d5ccee24
Swapnil <sb015002@gmail.com>
2024-08-12 23:44:24 +0530

    origin

--- DSA_notes/move_zeroes_to_end.cpp
+++ DSA_notes/move_zeroes_to_end.cpp
@@ -1,59 +1,70 @@
 #include <bits/stdc++.h>
 using namespace std;
 
-void moveZerosToEnd(int arr[], int n){
+void moveZerosToEnd(int arr[], int n)
+{
 
-int j = - 1; 
-// this loop takes us to first zero
-for(int i = 0; i<n; i++){
+    int j = -1;
+    // this loop takes us to first zero
+    for (int i = 0; i < n; i++)
+    {
 
-    if(arr[i] == 0){
-        j = i; 
-        break;
+        if (arr[i] == 0)
+        {
+            j = i;
+            break;
+        }
+    }
+
+    if (j == -1)
+    {
+        return;
+    }
+
+    for (int i = j + 1; i < n; i++)
+    {
+
+        if (arr[i] != 0)
+        {
+            swap(arr[i], arr[j]);
+            j++;
+        }
     }
 }
-
-if (  j == -1){
-    return ;
-}
-
-for(int i = j+1; i<n; i++){
-
-if(arr[i] != 0){
-    swap(arr[i], arr[j]);
-    j++;
-}
-}}
 
 // Tc O(N)
 //  Sc O(1)
 
-int main(){
+int main()
+{
 
-cout << "Enter the size of array " ;
+    cout << "Enter the size of array ";
 
-int n;
-cin >> n;
-int arr[n];
-cout << endl << "Enter the elements of array " << endl;
-for(int i=0; i<n; i++){
-    cin >> arr[i];
-}
-cout << endl;
+    int n;
+    cin >> n;
+    int arr[n];
+    cout << endl
+         << "Enter the elements of array " << endl;
+    for (int i = 0; i < n; i++)
+    {
+        cin >> arr[i];
+    }
+    cout << endl;
 
-cout << "Array before shifting" << endl;
+    cout << "Array before shifting" << endl;
 
-for(int i=0; i<n; i++){
-    cout << arr[i] << " ";
-}
+    for (int i = 0; i < n; i++)
+    {
+        cout << arr[i] << " ";
+    }
 
-moveZerosToEnd(arr, n);
+    moveZerosToEnd(arr, n);
 
-cout << endl << "Array after shifting" << endl;
+    cout << endl
+         << "Array after shifting" << endl;
 
-for(int i=0; i<n; i++){
-    cout << arr[i] << " ";
-}
-
-
+    for (int i = 0; i < n; i++)
+    {
+        cout << arr[i] << " ";
+    }
 }
